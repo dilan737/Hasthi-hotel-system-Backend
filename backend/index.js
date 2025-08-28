0e4ffdb480099d9f4a2d759276f470859ab7d63b
Dilan <dilanavishka070@gmail.com>
2025-08-29 01:50:01 +0530

    👍👍👍👍💅😊

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import express from "express"; 
+import bodyParser from "body-parser"
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import verifyJwt from "./middleware/auth.js";
+import orderrouter from "./routes/orderRoutes.js";
+import dotenv from "dotenv"
+dotenv.config()
+
+
+
+const app=express();
+
+
+//mongoose.connect("mongodb+srv://admin:1234@cluster0.sfdum3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(
+    ()=>{
+
+        console.log("Connected To the database");
+    }
+).catch(
+    ()=>{
+        console.log("Connection Failed");
+    }
+)
+
+// ).catch((err) => {
+//     console.error("Connection Failed:", err);
+// });
+
+app.use(bodyParser.json());
+app.use(verifyJwt);
+
+
+
+app.use("/api/user",userRouter);
+app.use("/api/product",productRouter);
+app.use("/api/order",orderrouter);
+
+
+
+app.listen(5000,
+    ()=>{
+        
+        console.log("server is Running on port 5000");
+    }
+)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { BrowserRouter, Route,Routes} from 'react-router-dom'
+
+import './App.css'
+ 
+
+import LoginPage from './Pages/loginpage'
+import AdminPage from './Pages/adminpage'
+import Testing from './Pages/testing'
+
+function App() {
+
+
+  return (
+     
+    <BrowserRouter>
+
+       <Routes path="/*">
+
+       <Route path="/admin/*" element={<AdminPage/>}/>
+       <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/testing" element={<Testing/>}/>
+       <Route path="/" element={<h1>Home</h1>}/>
+       <Route path="/*" element={<h1>404 Not Found</h1>}/>
+        
+    
+       
+       </Routes>
+    
+    
+    </BrowserRouter>
+    );
+}
+
+export default App

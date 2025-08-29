687667f84434fb63f4b6f9f5d010bb76991e8fe0
Dilan <dilanavishka070@gmail.com>
2025-08-29 10:49:26 +0530

    updated the file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,32 @@ import { BrowserRouter, Route,Routes} from 'react-router-dom'
 import './App.css'
  
 
-import LoginPage from './Pages/loginpage'
-import AdminPage from './Pages/adminpage'
-import Testing from './Pages/testing'
+// import LoginPage from './Pages/loginpage'
+// import AdminPage from './Pages/adminpage'
+// import Testing from './Pages/testing'
 
 function App() {
 
 
   return (
      
-    <BrowserRouter>
+    // <BrowserRouter>
 
-       <Routes path="/*">
+    //    <Routes path="/*">
 
-       <Route path="/admin/*" element={<AdminPage/>}/>
-       <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/testing" element={<Testing/>}/>
-       <Route path="/" element={<h1>Home</h1>}/>
-       <Route path="/*" element={<h1>404 Not Found</h1>}/>
+    //    <Route path="/admin/*" element={<AdminPage/>}/>
+    //    <Route path="/login" element={<LoginPage/>}/>
+    //     <Route path="/testing" element={<Testing/>}/>
+    //    <Route path="/" element={<h1>Home</h1>}/>
+    //    <Route path="/*" element={<h1>404 Not Found</h1>}/>
         
     
        
-       </Routes>
+    //    </Routes>
     
     
-    </BrowserRouter>
+    // </BrowserRouter>
+    sample
     );
 }
 
